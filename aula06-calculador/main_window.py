7d33a4302b58ea95f903a104ca5f5bc5755c22f4
WerlemDantas <156126100+WerlemDantas@users.noreply.github.com>
2024-01-09 16:16:14 -0300

    Add files via upload
    Criando um repositório para calculadora criada com Pyside6.

---
+++ aula06-calculador/main_window.py
@@ -0,0 +1,29 @@
+# Deus no comando
+from PySide6.QtWidgets import QMainWindow, QVBoxLayout, QWidget, QMessageBox
+from PySide6.QtGui import QIcon
+
+
+class MainWindow(QMainWindow):
+    def __init__(self, parent: QWidget | None = None, *args, **kwargs) -> None:
+        super().__init__(parent, *args, **kwargs)
+
+        # Configurando o layout básico
+        self.cw = QWidget()
+        self.vLayout = QVBoxLayout()
+        self.cw.setLayout(self.vLayout)
+        self.setCentralWidget(self.cw)
+
+        # Título da janela
+        self.setWindowTitle('Calculadora-WD')    
+
+    def adjustFixedSize(self):
+        # Última coisa a ser feita
+        self.adjustSize()
+        self.setFixedSize(self.width(), self.height())
+    def addWidgetToVLayout(self, widget: QWidget):
+        self.vLayout.addWidget(widget)
+    
+    def makeMsgBox(self):
+        return QMessageBox(self)
+        
+
